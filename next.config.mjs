5a003858b381bd9b70183485f69463a54351b20b
Hilll19 <hilworking0110@gmail.com>
2025-02-26 10:45:52 +0700

    feat : add domain images to dashboard with next config lh3.googleusercontent.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-      GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-    },
-  };
-  
-  export default nextConfig;
-  
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+  },
+  reactStrictMode: true,
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+};
+
+export default nextConfig;
