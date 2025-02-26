f40e5bd077655fb6b4068456fc10ae36b27e917d
Hilll19 <hilworking0110@gmail.com>
2025-02-26 08:22:26 +0700

    feat : aurh Provider to session children page

---
+++ src/components/AuthProvider.js
@@ -0,0 +1,7 @@
+"use client"; // Wajib agar bisa digunakan di client
+
+import { SessionProvider } from "next-auth/react";
+
+export default function AuthProvider({ children, session }) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+}
